47fd21d2a483a843f5f8f7382f0aa6151bb5f0a6
Dhiraj Kelhe <dhirajkelhe3114@gmail.com>
2020-10-31 16:28:53 +0530

    Updated DP Solution file

--- src/Knapsack_DP.py
+++ src/Knapsack_DP.py
@@ -1,14 +1,6 @@
-# A naive recursive implementation
-# of 0-1 Knapsack Problem
-
-# Returns the maximum value that
-# can be put in a knapsack of
-# capacity knapsack_threshold
-
 class Knapsack_Class_DP:
     def knapSack(self, knapsack_threshold, weight, value, count):
         K = [[0 for x in range(knapsack_threshold + 1)] for x in range(count + 1)]
-        # Build table K[][] in bottom up manner
         for i in range(count + 1):
             for w in range(knapsack_threshold + 1):
                 if i == 0 or w == 0:
@@ -21,10 +13,10 @@ class Knapsack_Class_DP:
         return K[count][knapsack_threshold]
 
 
-if __name__ == '__main__':
-    value = [60, 100, 120]
-    weight = [10, 20, 30]
-    knapsack_threshold = 50
-    count = len(value)
-    session_knapsack = Knapsack_Class_DP()
-    print(session_knapsack.knapSack(knapsack_threshold, weight, value, count))
+# if __name__ == '__main__':
+#     value = [60, 100, 120]
+#     weight = [10, 20, 30]
+#     knapsack_threshold = 50
+#     count = len(value)
+#     session_knapsack = Knapsack_Class_DP()
+#     print(session_knapsack.knapSack(knapsack_threshold, weight, value, count))
